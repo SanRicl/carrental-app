8348178a81ee111d505e35db5d5d7ef6998abd6b
Sanderson <riclsanderson1@gmail.com>
2024-05-13 09:32:17 -0300

    fix: setting files into repo

---
+++ components/index.tsx
@@ -0,0 +1,21 @@
+import Hero from './Hero'
+import CustomButton from './CustomButton'
+import NavBar from './NavBar'
+import Footer from './Footer'
+import SearchBar from './SearchBar'
+import SearchManufacturer from './SearchManufacturer'
+import CarCard from './CarCard'
+import CarDetails from './CarDetails'
+import ShowMore from './ShowMore'
+
+export {
+  Hero,
+  CustomButton,
+  Footer,
+  NavBar,
+  SearchBar,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+  ShowMore,
+}
